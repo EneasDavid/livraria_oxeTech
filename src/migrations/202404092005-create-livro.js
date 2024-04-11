d870636aa8d936418976e9edcd21ca4e7b70d6ab
David Enéas <70227448+EneasDavid@users.noreply.github.com>
2024-04-11 19:55:42 -0300

    edição de delite on cascade

--- src/migrations/202404092005-create-livro.js
+++ src/migrations/202404092005-create-livro.js
@@ -18,22 +18,30 @@ module.exports = {
       id_editora: {
         allowNull: false,
         type: Sequelize.INTEGER,
-        references: { model: 'editoras', key: 'id' }
+        references: { model: 'editoras', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
       },
       id_categoria: {
         allowNull: false,
         type: Sequelize.INTEGER,
-        references: { model: 'categorias', key: 'id' }
+        references: { model: 'categorias', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
       },
       id_criador: {
         allowNull: false,
         type: Sequelize.INTEGER,
-        references: { model: 'usuarios', key: 'id' }
+        references: { model: 'usuarios', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
       },      
       id_autor: {
         allowNull: false,
         type: Sequelize.INTEGER,
-        references: { model: 'autores', key: 'id' }
+        references: { model: 'autores', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
       },
       createdAt: {
         allowNull: false,
